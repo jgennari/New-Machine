0c6abfbb24d4c8410ba32d3dbb823ea75aa2ff30
Joey Gennari <jgennari@gmail.com>
2017-12-21 09:12:02 -0500

    Update BaseInstall.ps1

--- BaseInstall.ps1
+++ BaseInstall.ps1
@@ -1,4 +1,5 @@
- Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Unrestricted
 choco feature enable -n allowGlobalConfirmation
 
 choco install cmder
